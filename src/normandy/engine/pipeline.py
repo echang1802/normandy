78151d0fc2b11f82d3b4f09d20a7dd1394f46bec
= <eloy.chang.182@gmail.com>
2021-08-12 12:51:21 -0300

    Added params option to processes

--- src/normandy/engine/pipeline.py
+++ src/normandy/engine/pipeline.py
@@ -15,7 +15,7 @@ class pipeline:
             confs = load(file, Loader=SafeLoader)
 
             # Define flows with the givens tags
-            self.__flows__ = [self.flow(data, flow_name, self.tags) for flow_name, data in confs["flows"].items() if self.tags.intersection(set(data["tags"]))]
+            self.__flows__ = [flow(data, flow_name, self.tags) for flow_name, data in confs["flows"].items() if self.tags.intersection(set(data["tags"]))]
 
             # Read the project configurations
             self.__confs__ = confs["confs"]
@@ -44,6 +44,7 @@ class pipeline:
 
     def __process_runner__(self, process):
         # Run the selected process
+        print(process)
         process.execute(self)
 
     def __flow_runner__(self, flow):
@@ -65,80 +66,108 @@ class pipeline:
         for fl in self.__flows__:
             self.__flow_runner__(fl)
 
-    class flow:
+class flow:
 
-        def __init__(self, flow_data, name, tags):
-            self.__type__ = "flow"
-            self.__tags__ = flow_data["tags"]
-            self.__name__ = name
-            self.__steps__ = [self.step(data, step_name, self.__name__, tags) for step_name, data in flow_data["steps"].items()]
+    def __init__(self, flow_data, name, tags):
+        self.__type__ = "flow"
+        self.__tags__ = flow_data["tags"]
+        self.__name__ = name
+        self.__steps__ = [step(data, step_name, self.__name__, tags) for step_name, data in flow_data["steps"].items()]
 
-        def steps(self):
-            return self.__steps__
+    def steps(self):
+        return self.__steps__
 
-        def step_number(self):
-            return len(self.__steps__)
+    def step_number(self):
+        return len(self.__steps__)
 
-        def __str__(self):
-            return f"Flow: {self.__name__}"
+    def __str__(self):
+        return f"Flow: {self.__name__}"
 
-        class step:
+class step:
 
-            def __init__(self, step_data, name, belong, tags):
-                self.__type__ = "step"
-                self.__name__ = name
-                self.__from_flow__ = belong
-                if type(step_data) == dict:
-                    self.__processes__ = [self.process(process_data, process_name, self.__name__, self.__from_flow__) for process_name, process_data in step_data.items() if type(process_data) == list or (not "avoid_tags" in process_data.keys()) or (not tags.intersection(set(process_data["avoid_tags"])))]
-                else:
-                    self.__processes__ = [self.process(None, process_name, self.__name__, self.__from_flow__) for process_name in step_data]
+    def __init__(self, step_data, name, belong, tags):
 
-            def processes(self):
-                return self.__processes__
+        from normandy.engine.errors import definition_error
 
-            def processes_number(self):
-                return len(self.__processes__)
+        self.__type__ = "step"
+        self.__name__ = name
+        self.__from_flow__ = belong
+        try:
+            if type(step_data) == dict:
+                self.__parse_processes__(step_data, tags)
+            else:
+                self.__processes__ = [process(None, process_name, self.__name__, self.__from_flow__) for process_name in step_data]
+        except Exception as e:
+            raise definition_error(f"Bad process definition: {e}")
 
-            def __str__(self):
-                return f"Step from {self.__from_flow__} flow: {self.__name__}"
+    def __parse_processes__(self, step_data, tags):
+        self.__processes__ = []
+        for process_name, process_data in step_data.items():
+            if type(process_data) == dict and "avoid_tags" in process_data.keys() and tags.intersection(set(process_data["avoid_tags"])):
+                continue
+            if type(process_data) == dict and "iter_param" in process_data.keys():
+                param_name = process_data["iter_param"]["name"]
+                if not "params" in process_data.keys():
+                    process_data["params"] = {}
+                for param_value in process_data["iter_param"]["values"]:
+                    process_data["params"] = process_data["params"].copy()
+                    process_data["params"][param_name] = param_value
+                    self.__processes__.append(process(process_data, process_name, self.__name__, self.__from_flow__))
+            else:
+                self.__processes__.append(process(process_data, process_name, self.__name__, self.__from_flow__))
 
-            class process:
 
-                def __init__(self, process_data, name, step_name, flow_name):
-                    self.__type__ = "process"
-                    self.__data__ = process_data
-                    self.__name__ = name
-                    self.__from_step__ = step_name
-                    self.__from_flow__ = flow_name
-                    self.__error_tolerance__ = self.__setup__(process_data, "error_tolerance")
+    def processes(self):
+        return self.__processes__
 
-                def __setup__(self, data, setup):
-                    setups_defaults = {
-                        "error_tolerance" : False
-                    }
-                    if type(data) == dict and setup in data.keys():
-                        return data[setup]
-                    return setups_defaults[setup]
+    def processes_number(self):
+        return len(self.__processes__)
 
-                def execute(self, pipe):
-                    from datetime import datetime
-                    from importlib.util import spec_from_file_location, module_from_spec
-                    from normandy.engine.errors import step_error
-                    from normandy.engine.logger import logger
+    def __str__(self):
+        return f"Step from {self.__from_flow__} flow: {self.__name__}"
 
-                    _t = datetime.now()
-                    log = logger(self, pipe.__log_level__)
+class process:
 
-                    # Import and execute the user define module 
-                    spec = spec_from_file_location(f"{self.__from_step__}.{self.__name__}", f"{pipe.get_path()}/pipeline/{self.__from_step__}/{self.__name__}.py")
-                    module = module_from_spec(spec)
-                    spec.loader.exec_module(module)
-                    try:
-                        module.process(pipe, log)
-                    except Exception as e:
-                        if not self.__error_tolerance__:
-                            raise step_error("Step exception without errors tolerance")
-                        log.error(e)
+    def __init__(self, process_data, name, step_name, flow_name):
 
-                    log.info(f"Process successfully ended at {datetime.now()} - Process time: {datetime.now() - _t}")
-                    return
+        self.__type__ = "process"
+        self.__name__ = name
+        self.__from_step__ = step_name
+        self.__from_flow__ = flow_name
+        self.__error_tolerance__ = self.__setup__(process_data, "error_tolerance")
+        self.__params__ = self.__setup__(process_data, "params")
+
+    def __setup__(self, data, setup):
+        setups_defaults = {
+            "error_tolerance" : False,
+            "params" : {}
+        }
+        if type(data) == dict and setup in data.keys():
+            return data[setup]
+        return setups_defaults[setup]
+
+    def execute(self, pipe):
+        from datetime import datetime
+        from importlib.util import spec_from_file_location, module_from_spec
+        from normandy.engine.errors import process_error
+        from normandy.engine.logger import logger
+
+        _t = datetime.now()
+        log = logger(self, pipe.__log_level__)
+
+        # Import and execute the user define module
+        spec = spec_from_file_location(f"{self.__from_step__}.{self.__name__}", f"{pipe.get_path()}/pipeline/{self.__from_step__}/{self.__name__}.py")
+        module = module_from_spec(spec)
+        spec.loader.exec_module(module)
+        try:
+            module.process(pipe, log, self.__params__)
+        except Exception as e:
+            if not self.__error_tolerance__:
+                raise process_error("Process exception without errors tolerance")
+            log.error(e)
+
+        log.info(f"Process successfully ended at {datetime.now()} - Process time: {datetime.now() - _t}")
+        return
+
+    def __str__(self):
+        return f"Process from {self.__from_step__} step from {self.__from_flow__} flow: {self.__name__} -- Params: {self.__params__}"
